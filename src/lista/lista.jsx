5138d4742e5fdb54e6cc83ae96cc13df07583e6c
josemifd <josemifd20@gmail.com>
2024-10-22 12:09:11 +0200

    Cambio img de sitio

--- src/lista/lista.jsx
+++ src/lista/lista.jsx
@@ -1,112 +1,112 @@
 const lista = [
     {
         id: 1,
-        img: "/unizircle/src/assets/despachoprof.jpg",
+        img: "despachoprof.jpg",
         title: "Mesa de despacho",
         facultad: "Escuela de Ingeniería y Arquitectura",
         departamento: "Informática e Ingeniería de Sistemas"
     },
     {
         id: 2,
-        img: "/unizircle/src/assets/mesa-sala.jpg",
+        img: "mesa-sala.jpg",
         title: "Mesa sala de estudios",
         facultad: "Campus Paraíso",
         departamento: "Facultad de Economía y Empresa"
     },
     {
         id: 3,
-        img: "/unizircle/src/assets/adaptado.jpg",
+        img: "adaptado.jpg",
         title: "Mesa movilidad reducida",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Derecho"
     },
     {
         id: 4,
-        img: "/unizircle/src/assets/silla-sala.jpg",
+        img: "silla-sala.jpg",
         title: "x10 sillas sala estudios",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Ciencias"
     },
     {
         id: 5,
-        img: "/unizircle/src/assets/proyector.jpg",
+        img: "proyector.jpg",
         title: "Proyector aula",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Derecho"
     },
     {
         id: 6,
-        img: "/unizircle/src/assets/sillon_profesor.jpg",
+        img: "sillon_profesor.jpg",
         title: "Silla profesor",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Filosofía y Letras"
     },
     {
         id: 7,
-        img: "/unizircle/src/assets/ordenador.jpg",
+        img: "ordenador.jpg",
         title: "Ordenador sobremesa",
         facultad: "Escuela de Ingeniería y Arquitectura",
         departamento: "Informática e Ingeniería de Sistemas"
     },
     {
         id: 8,
-        img: "/unizircle/src/assets/silla_inf.jpeg",
+        img: "silla_inf.jpeg",
         title: "Sillas sala informática",
         facultad: "Escuela de Ingeniería y Arquitectura",
         departamento: "Informática e Ingeniería de Sistemas"
     },
     {
         id: 9,
-        img: "/unizircle/src/assets/sobremesa_ciencias.jpg",
+        img: "sobremesa_ciencias.jpg",
         title: "Ordenadores sobremesa",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Ciencias"
     },
     {
         id: 10,
-        img: "/unizircle/src/assets/pizarra.jpg",
+        img: "pizarra.jpg",
         title: "Pizarra aula",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Ciencias"
     },
     {
         id: 11,
-        img: "/unizircle/src/assets/micro.jpg",
+        img: "micro.jpg",
         title: "Microondas",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Educación"
     },
     {
         id: 12,
-        img: "/unizircle/src/assets/cartel.jpg",
+        img: "cartel.jpg",
         title: "Cartel para eventos",
         facultad: "Campus San Francisco",
         departamento: "Facultad de Filosofía y Letras"
     },
     {
         id: 13,
-        img: "/unizircle/src/assets/perchero.jpg",
+        img: "perchero.jpg",
         title: "Percheros aula",
         facultad: "Campus Río Ebro",
         departamento: "Edificio Betancourt"
     },
     {
         id: 14,
-        img: "/unizircle/src/assets/pantalla_proyector.jpg",
+        img: "pantalla_proyector.jpg",
         title: "Pantalla de proyección",
         facultad: "Campus San Francisco",
         departamento: "Facultado de Filosofía y Letras"
     },
     {
         id: 15,
-        img: "/unizircle/src/assets/mesa_reunion.webp",
+        img: "mesa_reunion.webp",
         title: "Mesa de reuniones",
         facultad: "Campus San Francisco",
         departamento: "Facultado de Filosofía y Letras"
     },
     {
         id: 16,
-        img: "/unizircle/src/assets/cafe.jpeg",
+        img: "cafe.jpeg",
         title: "Pizarra cafetería",
         facultad: "Campus Río Ebro",
         departamento: "Edificio Torres Quevedo"
